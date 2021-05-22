4f4c634a524ae95ac61c68c9a45413ac632b87bf
Viet D Nguyen <30382979+vdn19@users.noreply.github.com>
2021-05-22 23:46:11 +1000

    cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
+const cors = require("cors");
 const edith = require("./services/edith");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
