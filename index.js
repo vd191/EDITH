27006fa8462056254732d83ecae00021850c08e4
Viet D Nguyen <30382979+vdn19@users.noreply.github.com>
2021-05-22 23:23:48 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const edith = require("./services/edith");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
